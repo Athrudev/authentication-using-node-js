0c68d4e86f44de78509ae1722e546b37732d14b7
Atharva Kamble <138187173+Athrudev@users.noreply.github.com>
2024-08-29 10:40:18 +0000

    cookie setup complted

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const express = require('express')
-const app = express()
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.use(cookieParser());
 
-app.listen(3000)
+app.get('/', async function (req, res) {
+    res.cookie('demo', 'Athrudev');
+    res.send('Hello World');
+    console.log('Listening on the port localhost:3000');
+});
+
+app.get('/see', async function (req, res) {
+    console.log(req.cookies);
+    res.send('Check your console for cookies');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
