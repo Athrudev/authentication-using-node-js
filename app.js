c03d1f056fc7aa776a5918ba477f4904036a39c7
Atharva Kamble <138187173+Athrudev@users.noreply.github.com>
2024-08-29 10:04:02 +0000

    set the cookie and solved the error

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const cookieParser = require('cookie-parser')
-const express = require('express')
-const app = express()
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-  res.cookie('demo','Athrudev')
-  console.log('Listening on the port localhost:3000')
-})
+app.get('/', async function (req, res) {
+    res.cookie('demo', 'Athrudev');
+    res.send('Hello World');
+});
 
-app.listen(3000)
+app.get('/see', async function (req, res) {
+    console.log(req.cookies);
+    res.send('Check your console for cookies');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
