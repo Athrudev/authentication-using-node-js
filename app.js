298fc172edfdc5c0695634bf14222f0cee40e8b6
Atharva Kamble <138187173+Athrudev@users.noreply.github.com>
2024-08-29 10:18:48 +0000

    Merge branch 'new' into main

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const app = express();
-
-app.use(cookieParser());
-
-app.get('/', async function (req, res) {
-    res.cookie('demo', 'Athrudev');
-    res.send('Hello World');
-});
-
-app.get('/see', async function (req, res) {
-    console.log(req.cookies);
-    res.send('Check your console for cookies');
-});
-
-app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
-});
-=======
 const express = require('express')
 const app = express()
 
@@ -27,4 +6,3 @@ app.get('/', function (req, res) {
 })
 
 app.listen(3000)
->>>>>>> main
